48ae96d3c4d2619536634aa6124d8270d3828a29
isha-web1 <isha.web1@gmail.com>
2023-08-10 00:33:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import Header from './components/Header/Header'
+
+
+function App() {
+  
+
+  return (
+    <div className='body'>
+    <Header></Header>
+     
+    </div>
+  )
+}
+
+export default App
