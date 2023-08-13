9ccb71a60e279fa851abd9d6c2e8710bd1ba9a2e
isha-web1 <isha.web1@gmail.com>
2023-08-13 21:11:36 +0600

    calculate shopping cart

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -5,19 +5,23 @@ const Cart = (props) => {
     const cart = props.cart;
 
     let total = 0;
+    let totalShipping = 0;
     for(const product of cart){
         total = total + product.price;
+        totalShipping = totalShipping + product.shipping;
     }
 
+    const tax = total*7/100;
+    const grandTotal = total + totalShipping + tax;
 
   return (
     <div className="cart">
       <h1>order summery</h1>
       <h3>total order : {cart.length}</h3>
        <h3>total price :${total}</h3>
-       <h3>total shipping :$</h3>
-       <h3>tax :$</h3>
-       <h2>grand total :$</h2>
+       <h3>total shipping :${totalShipping}</h3>
+       <h3>tax :${tax.toFixed(2)}</h3>
+       <h2>grand total :${grandTotal.toFixed(2)}</h2>
     </div>
   );
 };
