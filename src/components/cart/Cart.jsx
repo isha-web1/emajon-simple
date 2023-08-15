ac830258c2ecbece72cbaba365eefc00ce3352f7
isha-web1 <isha.web1@gmail.com>
2023-08-15 12:28:00 +0600

    handle qunatity from storage

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -6,9 +6,12 @@ const Cart = (props) => {
 
     let total = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart){
-        total = total + product.price;
+       product.quantity = product.quantity || 1;
+        total = total + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
 
     const tax = total*7/100;
@@ -17,7 +20,7 @@ const Cart = (props) => {
   return (
     <div className="cart">
       <h1>order summery</h1>
-      <h3>total order : {cart.length}</h3>
+      <h3>total order : {quantity}</h3>
        <h3>total price :${total}</h3>
        <h3>total shipping :${totalShipping}</h3>
        <h3>tax :${tax.toFixed(2)}</h3>
